a9a63223e005fd6798242b584df20154605f1494
eduarxx1255 <coronaeduarxx@gmail.com>
2019-10-14 13:30:21 -0500

    se agrego componente publicaciones

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
+import './css/iconos.css';
 
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
